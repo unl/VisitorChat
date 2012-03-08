207d48546f255bc7b9045dd9f8d0b85a43945be5
Michael Fairchild <mfairchild365@gmail.com>
2012-03-08 13:28:42 -0600

    add comments.

--- src/UNL/VisitorChat/URL/Service.php
+++ src/UNL/VisitorChat/URL/Service.php
@@ -13,6 +13,14 @@ class Service
         $this->url = \UNL\VisitorChat\Controller::$url;
     }
     
+    /**
+     * Generates a site url based on a path, and will optionally appened the
+     * current format and phpsessid to the url.
+     * 
+     * @param string $path
+     * @param mixed $appendFormat
+     * @param bool $appendPhpsessid
+     */
     function generateSiteURL($path, $appendFormat = false, $appendPhpsessid = false)
     {
         $url = $this->url . $path;
@@ -28,6 +36,14 @@ class Service
         return $url;
     }
     
+    /**
+     * Generates a url with data from the parse_url function.
+     * 
+     * @param array $data
+     * @throws \Exception
+     * 
+     * @return string url
+     */
     private function generateURLWithURLData($data = array())
     {
         $url = "";
@@ -47,6 +63,17 @@ class Service
         return $data['scheme'] . "://" . $data['host'] . $data['path'];
     }
     
+    
+    /**
+     * Appends a format to to a url.  If the $format param is set to true
+     * it will appened the current format, otherwise it will append the value
+     * of $format.
+     * 
+     * @param string $url
+     * @param mixed $format
+     * 
+     * @return string $url
+     */
     function appendFormat($url, $format) {
         if ($format === true) {
             $format = $this->options['format'];
@@ -59,6 +86,15 @@ class Service
         return $url;
     }
     
+    /**
+     * Appends the current phpsessid to a url.  This function is 
+     * avaible so that servers that do not have the phpsessid auto appened
+     * can call this function.
+     * 
+     * @param string $url
+     * 
+     * @return string $url
+     */
     function appendPhpsessid($url)
     {
         if (!$url = $this->addParams($url, array('PHPSESSID' => session_id()))) {
@@ -68,6 +104,14 @@ class Service
         return $url;
     }
     
+    /**
+     * Add GET parameters to a url.
+     * 
+     * @param string $url
+     * @param array $params
+     * 
+     * @return string $url
+     */
     function addParams($url, $params) {
         if (!$data = parse_url($url)) {
             return false;
@@ -88,7 +132,15 @@ class Service
         return $url .= $query;
     }
     
-    private function queryToArray($query = array())
+    /**
+     * converts a query string built with parse_url to
+     * an array.
+     * 
+     * @param string $query
+     * 
+     * @return array $array
+     */
+    private function queryToArray($query)
     {
         $array = array();
         $pairs = explode("&", $query);
