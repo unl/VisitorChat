229593823a43b4d2b4926303d5cc69681b67638f
Caleb Wiedel <cbwiedel@huskers.unl.edu>
2012-03-29 15:45:25 -0500

    Fixed (again) minimize, logout, and header opacity functions

--- www/js/VisitorChat/Remote.js
+++ www/js/VisitorChat/Remote.js
@@ -64,11 +64,9 @@ var VisitorChat_Chat = VisitorChat_ChatBase.extend({
      * of watch functions, we should always unbind previous 
      * watch functions before applying the new ones.
      */
-    WDN.jQuery('#visitorChat_launcher, ' +
-            '#visitorChat_close, ' +
-            '#visitorChat_container, ' +
+    WDN.jQuery('#visitorChat_container, ' +
             '#visitorChat_email_fallback, ' +
-            '#visitorChat_collapse, ' +
+            '#visitorChat_logout, ' +
             '#visitorChat_login_sumbit, ' +
             '#visitorChat_header').unbind();
     
@@ -94,14 +92,26 @@ var VisitorChat_Chat = VisitorChat_ChatBase.extend({
     //Click header to open up Chat
     WDN.jQuery('#visitorChat_header').click(WDN.jQuery.proxy(function(){
       if (VisitorChat.chatOpened) {
-        if ((VisitorChat.chatStatus == 'CHATTING' || VisitorChat.chatStatus == 'OPERATOR_PENDING_APPROVAL') && !VisitorChat.confirmClose()) {
-          return false;
+    	if (VisitorChat.chatStatus == 'CHATTING' || VisitorChat.chatStatus == 'OPERATOR_PENDING_APPROVAL') {
+    	  if (WDN.jQuery('#visitorChat_container').css('display') === 'none') {
+    		WDN.jQuery("#visitorChat_container").slideDown(450);
+          } else {
+    	    WDN.jQuery("#visitorChat_container").slideUp(450);
+          }
+    	  return false;
         }
         VisitorChat.stop();
       } else {
         VisitorChat.startChat();
       }
         
+      return false;
+    }, this));
+    
+    //Logout function
+    WDN.jQuery('#visitorChat_logout').click(WDN.jQuery.proxy(function(){
+      if (!VisitorChat.confirmClose()) {return false;}
+      VisitorChat.stop();
       return false;
     }, this));
     
@@ -141,6 +151,11 @@ var VisitorChat_Chat = VisitorChat_ChatBase.extend({
     //This will slide down the Name and Email fields, plus the Ask button
     WDN.jQuery("#visitorChat_messageBox").keyup(function(){
         WDN.jQuery(".visitorChat_info, #visitorChat_login_sumbit").slideDown("fast");
+    });
+    
+    //Header is fully-visible when message box is focued
+    WDN.jQuery("#visitorChat_messageBox").focus(function(){
+      WDN.jQuery("#visitorChat_header").css({'opacity': '1'})
     });
   },
   
