1dcb1d07b85569acb3205f067a805af95fe93e2b
Michael Fairchild <mfairchild365@gmail.com>
2012-04-16 15:28:50 -0500

    Add some more details explaining the structure of the expected results for getting a list of sites for a given url.

--- src/UNL/VisitorChat/OperatorRegistry/DriverInterface.php
+++ src/UNL/VisitorChat/OperatorRegistry/DriverInterface.php
@@ -15,6 +15,11 @@ interface DriverInterface
 
     /**
      * Get the list of members for a site
+     * Note: The system expects to recieve a list of sites that relate to the given url.
+     * The first result should be the closest registered site, and each result thereafter
+     * should continue to get further away from the requested url.  This is so that
+     * when finding an operator to assign, the assignment will fallback on the closest 
+     * related site first if no one closer can be found.
      * 
      * @param string $site
      * 
