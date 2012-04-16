e4c8f0ba9cb9225879783c122ea98aff03f0bfd7
Michael Fairchild <mfairchild365@gmail.com>
2012-04-16 13:05:42 -0500

    use the getUID method to get the meber's uid, and return false if they are not at least a basic operator.

--- src/UNL/VisitorChat/User/Service.php
+++ src/UNL/VisitorChat/User/Service.php
@@ -6,7 +6,11 @@ class Service
     public static function areUsersAvaiable($users)
     {
         foreach ($users as $user) {
-            if (!$user = \UNL\VisitorChat\User\Record::getByUID($user)) {
+            if ($user->getRole() == 'other') {
+                continue;
+            }
+            
+            if (!$user = \UNL\VisitorChat\User\Record::getByUID($user->getUID())) {
                 continue;
             }
             
