e24784e9359bccf3637e7bf71427ee4609845aeb
Michael Fairchild <mfairchild365@gmail.com>
2012-04-16 11:10:14 -0500

    send the member uid, roles and site when constructing a new site member from the site memberlist.

--- src/UNL/VisitorChat/OperatorRegistry/WDN/Site/MemberList.php
+++ src/UNL/VisitorChat/OperatorRegistry/WDN/Site/MemberList.php
@@ -3,8 +3,17 @@ namespace UNL\VisitorChat\OperatorRegistry\WDN\Site;
 
 class MemberList extends \ArrayIterator implements \UNL\VisitorChat\OperatorRegistry\SiteMembersIterator
 {
-    function __construct($members)
+    public $site;
+    
+    function __construct($site, $members)
     {
+        $this->site = $site;
+        
         parent::__construct(new \ArrayIterator($members));
+    }
+    
+    function current() {
+        $data = parent::current();
+        return new Member(parent::key(), $data['roles'], $this->site);
     }
 }
