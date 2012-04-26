148252f8096227e6f02c033d4af7118cb586b921
Michael Fairchild <mfairchild365@gmail.com>
2012-04-26 14:44:16 -0500

    ONLY decode line breaks.

--- www/templates/default/UNL/VisitorChat/Message/View.tpl.php
+++ www/templates/default/UNL/VisitorChat/Message/View.tpl.php
@@ -1,3 +1,5 @@
 <?php 
-echo htmlspecialchars_decode($context->message) . "<br /><span class='timestamp'>" . date("g:i:s A", strtotime($context->date_created)) . "</span><span class='stamp'>from " . $context->getPoster()->name . "</span>";
+$message = str_replace("&lt;br /&gt;", "<br />", $context->message);
+
+echo $message . "<br /><span class='timestamp'>" . date("g:i:s A", strtotime($context->date_created)) . "</span><span class='stamp'>from " . $context->getPoster()->name . "</span>";
 ?>
