ed2aa503d6f48cfc9a90c2a71156515b52bf4dfd
Michael Fairchild <mfairchild365@gmail.com>
2012-08-20 16:15:47 -0500

    add a script to try to process all email conversations if they are still searching.

---
+++ scripts/handleSearchingEmailConversations.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This script will clean up old idle conversations with a chat method of 'EMAIL' that have not been finished (are stuck on the 'searching')
+ * status.  In order for these emails to be sent and the conversations to be closed, at least 1 \UNL\VisitorChat\Conversation\Email::$fallbackEmails email address must be defined.
+ */
+
+if (file_exists(dirname(dirname(__FILE__)) . '/config.inc.php')) {
+    require_once dirname(dirname(__FILE__)) . '/config.inc.php';
+} else {
+    require dirname(dirname(__FILE__)) . '/config.sample.php';
+}
+
+foreach (\UNL\VisitorChat\Conversation\RecordList::getAllSearchingEmailConversations() as $conversation) {
+    $service = new \UNL\VisitorChat\Invitation\Service();
+    $result = $service->handleInvitations($conversation);
+    $conversation =  \UNL\VisitorChat\Conversation\Record::getByID($conversation->id);
+    echo $conversation->id . ": new status - " . $conversation->status . PHP_EOL;
+}
