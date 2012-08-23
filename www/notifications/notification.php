623cc87c073865f55300402d0f93d8186ce13260
Michael Fairchild <mfairchild365@gmail.com>
2012-08-23 10:27:28 -0500

    Use html notifications and display a larger flashing notification to operators.

---
+++ www/notifications/notification.php
@@ -0,0 +1,78 @@
+<?php
+if (file_exists(dirname(dirname(dirname(__FILE__))) . '/config.inc.php')) {
+    require_once dirname(dirname(dirname(__FILE__))) . '/config.inc.php';
+} else {
+    require dirname(dirname(dirname(__FILE__))) . '/config.sample.php';
+}
+
+$message = "Alert!";
+if (isset($_GET['message'])) {
+    $message = $_GET['message'];
+}
+?>
+
+<html>
+<head>
+    <title>New Assignment</title>
+    <script type="text/javascript" src="<?php echo \UNL\VisitorChat\Controller::$url?>js\jquery.min.js"></script>
+    <style type="text/css">
+        body {
+            height: 140px;
+            width: 280px;
+            text-align: center;
+        }
+        
+        h2 {
+            margin-bottom: 5px;
+        }
+
+        h3 {
+            margin-top: 5px;
+            margin-bottom: 5px;
+        }
+        
+        #notification {
+            background-color: rgba(255,255,255,0.7);
+            width: 260px;
+            height: 130px;
+            margin-left: auto;
+            margin-right: auto;
+            border-radius: 15px;
+            border: 2px solid black;
+        }
+        
+        #details {
+            font-size: 10px;
+        }
+    </style>
+    
+    <script type="text/javascript">
+        $(document).ready(function() {
+            startflashing();
+        });
+        
+        function startflashing(newCode) {
+            if (newCode == '#000000' || newCode == undefined) {
+                newCode = '#E01B1B';
+                timeout = 500;
+            } else {
+                newCode = '#000000';
+                timeout = 1500;
+            }
+            
+            $("body").css("background-color", newCode);
+            
+            setTimeout("startflashing('"+newCode+"')",timeout);
+        }
+    </script>
+</head>
+<body>
+<div id='notification'>
+    <h2>UNL Chat System</h2>
+    <h3><h3><?php echo $message ?></h3></h3>
+    <div id='details'>
+        (Click this notification to take you to the chat system)
+    </div>
+</div>
+</body>
+</html>
