9cb8555d2c63f67937b1ff79a8bafde644e677f0
Michael Fairchild <mfairchild365@gmail.com>
2012-09-06 11:36:50 -0500

    merge the popup notification setting into the user settings form.

--- www/templates/default/UNL/VisitorChat/User/Edit.tpl.php
+++ www/templates/default/UNL/VisitorChat/User/Edit.tpl.php
@@ -25,18 +25,8 @@ function get_var($var, $context) {
                 <label for="visitorchat_maxChats">Max Chats</label>
                 <input type="text" name="max_chats" id="max_chats" value="<?php echo get_var('max_chats', $context);?>"/>
             </li>
-        </ul>
-    </fieldset>
-    <input id='visitorChat_login_sumbit' type="submit" value="Submit" name="visitorChat_login_sumbit" />
-</form>
-
-<div>
-    <h3 class='zenform'>Notification settings</h3>
-    <form id='visitorchat_notifications' class='zenform' name='input' method="post" action="<?php  echo \UNL\VisitorChat\Controller::$URLService->generateSiteURL("user/settings", false, false);?>" >
-    <fieldset>
-        <ul>
             <li>
-                <label for="visitorchat_maxChats">Max Chats</label>
+                <label for="popup_notifications">Popup Notifications</label>
                 <select name="popup_notifications" id="popup_notifications">
                     <option value="1" <?php echo (get_var('popup_notifications', $context)==1)?'selected="selected"':'';?>>On</option>
                     <option value="0" <?php echo (get_var('popup_notifications', $context)==0)?'selected="selected"':'';?>>Off</option>
@@ -44,9 +34,10 @@ function get_var($var, $context) {
             </li>
         </ul>
     </fieldset>
-    <input type="submit" value="Submit" name="visitorChat_login_sumbit" />
+    <input id='visitorChat_login_sumbit' type="submit" value="Submit" name="visitorChat_login_sumbit" />
 </form>
-    
+
+<div>
     <div id="notificationOptions">
         <a href="#" id="requestNotifications">Show Desktop Notifications</a>
     </div>
