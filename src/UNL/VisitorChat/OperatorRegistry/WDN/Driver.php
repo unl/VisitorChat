6054201e6de9bf7875a8496ab9e2230810b207c0
Michael Fairchild <mfairchild365@gmail.com>
2012-09-24 16:35:26 -0500

    safely remove items from the array.

--- src/UNL/VisitorChat/OperatorRegistry/WDN/Driver.php
+++ src/UNL/VisitorChat/OperatorRegistry/WDN/Driver.php
@@ -62,15 +62,25 @@ class Driver extends \UNL\VisitorChat\CacheableURL implements \UNL\VisitorChat\O
         //All sites for a user.
         $sites = $this->query($user, $doNotCache);
         
+        //Create an array of all sites that need to be removed.
+        $unsets = array();
         foreach ($sites as $index=>$site) {
+            echo $index . " : " . $site->getTitle() . PHP_EOL;
             foreach ($site->getMembers() as $member) {
                 if ($member->getUID() == \UNL\VisitorChat\User\Service::getCurrentUser()->uid) {
                     //Remove this site from the site list IF the user is not a chat user.
+                    echo $site->getTitle() . "  " . $member->getRole() . PHP_EOL;
                     if ($member->getRole() == 'other') {
-                        $sites->offsetUnset($index);
+                        $unsets[] = $index;
+                        continue 2;
                     }
                 }
             }
+        }
+        
+        //remove the sites.
+        foreach ($unsets as $index) {
+            $sites->offsetUnset($index);
         }
         
         //re-cache the results
@@ -78,7 +88,7 @@ class Driver extends \UNL\VisitorChat\CacheableURL implements \UNL\VisitorChat\O
             //Set the cache.
             $this->setCache($cachePath, $sites);
         }
-
+        
         return new SiteList($sites);
     }
 
@@ -94,7 +104,8 @@ class Driver extends \UNL\VisitorChat\CacheableURL implements \UNL\VisitorChat\O
         //get all sites
         $sites = $this->query('*', $doNotCache);
 
-        //
+        //create an array of all sites that need to be unset
+        $unsets = array();
         foreach ($sites as $index=>$site) {
             foreach ($site->getMembers() as $member) {
                 //Remove this site from the site list IF the user is not a chat user.
@@ -103,6 +114,12 @@ class Driver extends \UNL\VisitorChat\CacheableURL implements \UNL\VisitorChat\O
                 }
                 
             }
+            
+            $unsets[] = $index;
+        }
+
+        //remove the sites.
+        foreach ($unsets as $index) {
             $sites->offsetUnset($index);
         }
 
