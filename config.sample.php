be0f49e316e42898ed473ce1a0728e911957bbdd
Michael Fairchild <mfairchild365@gmail.com>
2012-11-09 15:27:19 -0600

    Update the readme and sample config

--- config.sample.php
+++ config.sample.php
@@ -34,37 +34,76 @@ error_reporting(E_ALL);
 /**********************************************************************************************************************
  * General settings
  */
-//Change this to the full base url of this instance.
-\UNL\VisitorChat\Controller::setURL('http://ucommfairchild.unl.edu/visitorchat/www/');
 
-//Configure the allowed domains
-\UNL\VisitorChat\Controller::$allowedDomains = array('unl.edu', 'throughtheeyes.org', 'quiltstudy.org', 'digital-community.com', 'huskeralum.org');
+/* Change this to the full base url of this instance.
+ * 
+ * @param string - the full url to this instance.
+ */
+\UNL\VisitorChat\Controller::setURL('http://www.mysite.edu/'); //Training slash is required.
 
-//Current environment of the chat service (PRODUCTION, PHPT, DEV).
+/* Change this to a directory where you want temporary files to be stored.
+ * 
+ * @var string - absolute path to the temporary directory
+ */
+\UNL\VisitorChat\CacheableURL::$tmpDir = dirname(__FILE__) . "/tmp/";  //Must be writable
+
+/* Configure the allowed domains
+ * 
+ * @var array - an array of domains where chats are allowed to be started.
+ */
+\UNL\VisitorChat\Controller::$allowedDomains = array('mysite.edu', 'mysite2.org');
+
+/* Current environment of the chat service
+ * PRODUCTION - Live production.
+ * PHPT       - Unit Testing environment
+ * DEV        - Develop environment
+ * @var string - the current environment (PRODUCTION|PHPT|DEV)
+ */
 \UNL\VisitorChat\Controller::$environment = "PRODUCTION";
 
-//How often javascript clients should refresh (both operator and client)
+/* The refresh rate (operator and client)
+ * This describes the rate at with the js clients refresh in milliseconds.
+ * 
+ * @var int - the number of milliseconds to refresh
+ */
 \UNL\VisitorChat\Controller::$refreshRate = 2000;  //(every 2 seconds)
 
 //TTL for pending chat requests
 \UNL\VisitorChat\Controller::$chatRequestTimeout = 10000;;  //(10 seconds)
 
-//Set the fallback URLs (conversations will be routed to these operatoes if no one is available).
+/* Set fallback URLs
+ * Conversations that fail to be answered will fall back to these sites.
+ * 
+ * @var \ArrayIterator - an \ArrayIterator of sites.
+ */
 \UNL\VisitorChat\Controller::$fallbackURLs = new \ArrayIterator(array('http://ucommfairchild.unl.edu'));
 
 //Set session key to prevent man in the middle attacks.
-\UNL\VisitorChat\Controller::$sessionKey = "totallyCoolKey";
+/* Session key
+ * Helps to prevent man in the middle attacks.  It is important that you replace this value.
+ * 
+ * @var string
+ */
+\UNL\VisitorChat\Controller::$sessionKey = "replace me"; //!REPLACE ME!
 
-//Cache routes (does not auto refresh cache).
-\Epoch\Controller::$cacheRoutes = false;
+/* Caches routes (does not auto refresh cache)
+ * 
+ * @var bool
+ */
+\UNL\VisitorChat\Controller::$cacheRoutes = false;
 
-//To cache and minimize or not to cache and minimize javascrip.  That is the question.
+/* To cache and minimize or not to cache and minimize javascrip.  That is the question.
+ * 
+ * @var bool
+ */
 \UNL\VisitorChat\Controller::$cacheJS = false;
 
-//A list of admins (UIDs). Admins are able to view all past conversations.
+/* A list of admins (via their UIDs).  Admins can view all past conversations for all sites.
+ * 
+ * @var array - an array of admins where the value is the uid of a user.
+ */
 \UNL\VisitorChat\Controller::$admins = array('s-mfairch4');
 
-\UNL\VisitorChat\CacheableURL::$tmpDir = dirname(__FILE__) . "/tmp/";
 /**********************************************************************************************************************
  * Email settings
  */
@@ -78,10 +117,16 @@ error_reporting(E_ALL);
 \UNL\VisitorChat\Conversation\Email::$default_subject  = 'UNL Visitor Chat System';
 
 /**********************************************************************************************************************
- * Operator Registry settings
+ * Operator Registry settings (Required)
+ * 
+ * If routing of chats is going to work, the system needs to know who is assigned to what site... thus we need 
+ * a registry service.
+ * 
+ * We have our own registry service here at UNL.  You are welcome to use your own or make one.
+ * If you want to make your own, use the MockRegistryDriver class as a reference.
  */
 //Uncomment to set the registry service to something other than the default.
-#\UNL\VisitorChat\Controller::$registryService = new MockRegistryDriver();;
+#\UNL\VisitorChat\Controller::$registryService = new MockRegistryDriver();
 
 //WDN Registry service url.
 \UNL\VisitorChat\OperatorRegistry\WDN\Driver::$baseURI = "http://www1.unl.edu/wdn/registry/";
