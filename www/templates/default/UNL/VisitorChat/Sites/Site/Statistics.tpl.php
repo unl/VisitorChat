701e1c7326c6880a58d711b8f0a1b0f7b808961c
Michael Fairchild <mfairchild365@gmail.com>
2012-12-05 09:16:43 -0600

    remove dependency  on google js library

--- www/templates/default/UNL/VisitorChat/Sites/Site/Statistics.tpl.php
+++ www/templates/default/UNL/VisitorChat/Sites/Site/Statistics.tpl.php
@@ -1,49 +1,43 @@
 <script type="text/javascript" src="<?php echo \UNL\VisitorChat\Controller::$url; ?>js/timeline/timeline-min.js"></script>
 <script type="text/javascript" src="<?php echo \UNL\VisitorChat\Controller::$url; ?>js/moment.min.js"></script>
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
-    
+
 <link rel="stylesheet" type="text/css" href="<?php echo \UNL\VisitorChat\Controller::$url; ?>js/timeline/timeline.css">
 <link rel="stylesheet" type="text/css" href="<?php echo \UNL\VisitorChat\Controller::$url; ?>css/timeline.css">
 
 <script type="text/javascript">
-    google.load("visualization", "1");
-
-    // Set callback to run when API is loaded
-    google.setOnLoadCallback(drawVisualization);
-
     // Called when the Visualization API is loaded.
-    function drawVisualization() {
+    WDN.jQuery(function(){
         var original_data = <?php echo json_encode($context->getRawObject()->getStatusStatistics()); ?>
-        
+
         // Create and populate a data table.
         var data = [];
         var min  = false;
         var max  = false;
-        
+
         for (item in original_data) {
             if (!min) {
                 min = new Date(original_data[item]['start'] - 86400000);
             }
-            
+
             var num = original_data[item]['total'];
             var maxNum = 20;
-            
+
             var color = 'red';
-            
+
             if (num > 0) {
                 color = 'grey';
             }
-            
+
             //Calculate a human readable difference
             var diff = moment.duration(moment(original_data[item]['start']).diff(moment(original_data[item]['end']))).humanize();
-            
+
             height = Math.round(num / maxNum * 70 + 20);
             style = 'height:' + height + 'px;' +
                     'background-color: ' + color + ';';
-                    
+
             var content = '<div class="bar" style="' + style + '" ' +
                     ' title="' + num + ' people Available for ' + diff + '">' + num + '</div>';
-            
+
             //add to array
             data.push({
                 'start': new Date(original_data[item]['start']),
@@ -73,7 +67,8 @@
 
         // Draw our timeline with the created data and options
         timeline.draw(data, options);
-    }
+    });
+        
 </script>
     
 <div id="mytimeline"></div>
