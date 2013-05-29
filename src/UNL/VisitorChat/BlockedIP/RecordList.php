894fe9734b17154d2494d31a4ab5d2a4a6a86e57
Michael Fairchild <mfairchild365@gmail.com>
2013-05-29 10:30:57 -0500

    handle filter requests

--- src/UNL/VisitorChat/BlockedIP/RecordList.php
+++ src/UNL/VisitorChat/BlockedIP/RecordList.php
@@ -12,12 +12,8 @@ class RecordList extends \Epoch\RecordList
         \UNL\VisitorChat\Controller::requireOperatorLogin();
 
         $options['returnArray'] = true;
-        
-        if (isset($options['ip_address'])) {
-            $options['array'] = self::getALLActive($options);
-        } else {
-            //Return a list of all currently active blocks
-        }
+
+        $options['array'] = self::getByOptions($options);
 
         parent::__construct($options);
     }
@@ -65,6 +61,36 @@ class RecordList extends \Epoch\RecordList
                            WHERE NOW() BETWEEN blocked_ips.start_date and blocked_ips.end_date
                                AND blocked_ips.status = 'ENABLED'
                            ORDER BY blocked_ips.date_created ASC";
+
+        return self::getBySql($options);
+    }
+    
+    public static function getByOptions($options = array())
+    {
+        //Build the list
+        $options = $options + array(
+                'state' => 'active'
+            );
+        
+        $options = $options + self::getDefaultOptions();
+        
+        $options['sql'] = "SELECT blocked_ips.id
+                           FROM blocked_ips";
+        
+        switch ($options['state']) {
+            case 'active':
+                $options['sql'] .= " WHERE NOW() BETWEEN blocked_ips.block_start and blocked_ips.block_end
+                                        AND blocked_ips.status = 'ENABLED'";
+                break;
+            case 'inactive':
+                $options['sql'] .= " WHERE NOW() NOT BETWEEN blocked_ips.block_start and blocked_ips.block_end
+                                        OR  blocked_ips.status = 'DISABLED'";
+                break;
+            case 'all':
+                break;
+        }
+
+        $options['sql'] .= " ORDER BY blocked_ips.date_created ASC";
 
         return self::getBySql($options);
     }
