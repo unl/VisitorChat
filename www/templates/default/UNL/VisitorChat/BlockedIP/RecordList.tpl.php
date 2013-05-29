ea52d5afb3dad3ac12853e6a63f33b0740923d73
Michael Fairchild <mfairchild365@gmail.com>
2013-05-29 10:30:45 -0500

    add a filters form

--- www/templates/default/UNL/VisitorChat/BlockedIP/RecordList.tpl.php
+++ www/templates/default/UNL/VisitorChat/BlockedIP/RecordList.tpl.php
@@ -1,12 +1,40 @@
-View a list of blocked IP addresses
+<h3>Blocked IP addresses</h3>
+
+<script type="text/javascript">
+    //<![CDATA[
+    WDN.jQuery(document).ready(function(){
+        WDN.initializePlugin('zenform');
+        WDN.jQuery('.datepicker').datepicker();
+    });
+    //]]>
+</script>
+
+<h3 class='zenform'>Filters</h3>
+<form class='zenform' method="get" action="<?php echo \UNL\VisitorChat\Controller::$URLService->generateSiteURL("blocks");?>" >
+    <fieldset>
+        <ul>
+            <li>
+                <label for="ip_address">IP address:</label>
+                <input type="text" name="ip_address" id="ip_address" value="<?php echo (isset($_GET['ip_address']))?$_GET['ip_address']:'' ?>" />
+
+                <label for="enabled">Current State:</label>
+                <select name="state" id="state">
+                    <option value="active" <?php echo (isset($_GET['state']) && $_GET['state'] == 'active')?'selected="selected"':'';?>>Active</option>
+                    <option value="inactive" <?php echo (isset($_GET['state']) && $_GET['state'] == 'inactive')?'selected="selected"':'';?>>Inactive</option>
+                    <option value="all" <?php echo (isset($_GET['state']) && $_GET['state'] == 'all')?'selected="selected"':'';?>>All</option>
+                </select>
+                <input type="submit" value="Submit" />
+            </li>
+        </ul>
+    </fieldset>
+</form>
 
 <ul>
     <?php
     foreach ($context as $block) {
         ?>
-        <li><a href="<?php echo $block->getEditURL() ?>"><?php echo $block->ip_address ?></a></li>
+        <li><a href="<?php echo $block->getEditURL() ?>"><?php echo $block->ip_address ?></a> ends at <?php echo $block->block_end ?></li>
         <?php
     }
     ?>
-    
 </ul>
