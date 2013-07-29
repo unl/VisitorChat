d208f6a62ce34d2e47a311ad5504873322551958
Michael Fairchild <mfairchild365@gmail.com>
2013-07-29 14:41:12 -0500

    Remove the old-style popup notifications
    They no longer work...

--- www/templates/default/UNL/VisitorChat/User/Edit.tpl.php
+++ www/templates/default/UNL/VisitorChat/User/Edit.tpl.php
@@ -28,13 +28,6 @@ function get_var($var, $context) {
                 <label for="max_chats">Max Chats</label><br />
                 <input type="text" name="max_chats" id="max_chats" value="<?php echo get_var('max_chats', $context);?>"/>
             </li>
-            <li>
-                <label for="popup_notifications">Popup Notifications</label><br />
-                <select name="popup_notifications" id="popup_notifications">
-                    <option value="1" <?php echo (get_var('popup_notifications', $context)==1)?'selected="selected"':'';?>>On</option>
-                    <option value="0" <?php echo (get_var('popup_notifications', $context)==0)?'selected="selected"':'';?>>Off</option>
-                </select> 
-            </li>
         </ul>
     </fieldset>
     <input id='visitorChat_login_sumbit' type="submit" value="Submit" name="visitorChat_login_sumbit" />
