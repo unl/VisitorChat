3517b504e04bb96b58e23b5dd03a81a58ccf0655
Michael Fairchild <mfairchild365@gmail.com>
2013-09-11 14:14:54 -0500

    Don't open links in a new window

--- lib/Epoch/src/Epoch/Controller.php
+++ lib/Epoch/src/Epoch/Controller.php
@@ -205,6 +205,6 @@ class Controller
     }
 
     public static function makeClickableLinks($text) {
-        return preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#+%-]*(\?\S+)?[^\.\s])?)?)@', '<a href="$1" target="_blank">$1</a>', $text);
+        return preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#+%-]*(\?\S+)?[^\.\s])?)?)@', '<a href="$1">$1</a>', $text);
     }
 }
