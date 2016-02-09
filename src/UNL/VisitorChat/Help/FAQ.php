6ab5f9a270b26ea719ac432a02693a8d2161dc7e
Michael Fairchild <mfairchild365@gmail.com>
2016-02-09 11:06:27 -0600

    Redirect the FAQ

--- src/UNL/VisitorChat/Help/FAQ.php
+++ src/UNL/VisitorChat/Help/FAQ.php
@@ -5,6 +5,7 @@ class FAQ
 {
    public function __construct($options = array())
    {
+       \UNL\VisitorChat\Controller::redirect('http://wdn.unl.edu/unl-chat-faq');
        \UNL\VisitorChat\Controller::$pagetitle = "FAQ";
    }
 }
