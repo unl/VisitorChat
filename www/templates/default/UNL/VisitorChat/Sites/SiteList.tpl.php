7005078de8b9e44c536d395293c3098b23771e0d
Michael Fairchild <mfairchild365@gmail.com>
2016-05-05 11:10:07 -0500

    Move 'My Sites' to 4.1

--- www/templates/default/UNL/VisitorChat/Sites/SiteList.tpl.php
+++ www/templates/default/UNL/VisitorChat/Sites/SiteList.tpl.php
@@ -1,40 +1,31 @@
-<?php
-$i = 1;
-foreach ($context->sites as $site) {
-    // Reset $i to keep grid
-	if ($i > 3) {
-	    $i = 1;
-	}
-	
-	$role = "none";
-    
-    foreach ($site->getMembers() as $member) {
-        if ($member->getUID() != \UNL\VisitorChat\User\Service::getCurrentUser()->uid) {
-            continue;
+<div class="bp960-wdn-grid-set-halves wdn-grid-clear wdn-center centered">
+    <?php foreach ($context->sites as $site): ?>
+        <?php
+        $role = "none";
+        foreach ($site->getMembers() as $member) {
+            if ($member->getUID() != \UNL\VisitorChat\User\Service::getCurrentUser()->uid) {
+                continue;
+            }
+            
+            $role = $member->getRole();
+        }
+
+        $class = 'busy';
+        if ($site->getAvailableCount()) {
+            $class='available';
         }
         
-        $role = $member->getRole();
-    }
-	
-	if ($i == 1) {
-        echo "<section class='grid4 first'>";
-    } else {
-		echo "<section class='grid4'>";
-	}
-    
-    $class = 'busy';
-    if ($site->getAvailableCount()) {
-        $class='available';
-    }
-    
-    echo "<div class='zenbox bright $class'>
-	          <h3><a href='" . $site->getURL() . "'>" . $site->getTitle() . "</a>".
-		         "<a class='zen-header-link' href='" .
-		         \UNL\VisitorChat\Controller::$URLService->generateSiteURL('sites/site?url=' . urlencode($site->getURL())) . "'>View Details</a>".
-			 "</h3>".
-         "<ul>
-             <li>Your chat role: <strong>" . $role . "</strong></li>" .
-         "</ul></div></section>";
-    $i++;
-}
-?>
+        ?>
+        <div class="wdn-col">
+            <div class='visual-island site-box <?php echo $class ?>'>
+                <h2>
+                    <a href="zen-header-link" href="<?php echo $site->getURL()  ?>"><?php echo $site->getTitle() ?></a>
+                </h2>
+                <a href="<?php echo \UNL\VisitorChat\Controller::$URLService->generateSiteURL('sites/site?url=' . urlencode($site->getRawObject()->getURL())) ?>">View Details</a>
+                <p>
+                    Your chat role: <strong><?php echo $role ?></strong>
+                </p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
