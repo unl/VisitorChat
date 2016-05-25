991e4793c96580b38f9df82c68eb068ecde6e570
Michael Fairchild <mfairchild365@gmail.com>
2016-05-25 14:02:01 -0500

    clean up the user settings form

--- www/templates/default/UNL/VisitorChat/User/Edit.tpl.php
+++ www/templates/default/UNL/VisitorChat/User/Edit.tpl.php
@@ -8,28 +8,17 @@ function get_var($var, $context) {
 }
 ?>
 
-<script type="text/javascript">
-    //<![CDATA[
-    WDN.jQuery(document).ready(function(){
-        WDN.initializePlugin('zenform');
-    });
-    //]]>
-</script>
-
-<h3 class='zenform'>User settings</h3>
 <form id='visitorchat_maxChats' class='zenform' name='input' method="post" action="<?php  echo \UNL\VisitorChat\Controller::$URLService->generateSiteURL("user/settings", false, false);?>" >
-    <fieldset>
-        <ul>
-            <li>
-                <label for="alias">Alias (the name that will be displayed to clients.  If empty, your first name will be displayed.</label><br />
-                <input type="text" name="alias" id="alias" value="<?php echo get_var('alias', $context);?>"/>
-            </li>
-            <li>
-                <label for="max_chats">Max Chats</label><br />
-                <input type="text" name="max_chats" id="max_chats" value="<?php echo get_var('max_chats', $context);?>"/>
-            </li>
-        </ul>
-    </fieldset>
+    <ul>
+        <li>
+            <label for="alias">Alias (the name that will be displayed to clients.  If empty, your first name will be displayed.</label><br />
+            <input type="text" name="alias" id="alias" value="<?php echo get_var('alias', $context);?>"/>
+        </li>
+        <li>
+            <label for="max_chats">Max Chats</label><br />
+            <input type="text" name="max_chats" id="max_chats" required value="<?php echo get_var('max_chats', $context);?>"/>
+        </li>
+    </ul>
     <input id='visitorChat_login_sumbit' type="submit" value="Submit" name="visitorChat_login_sumbit" />
 </form>
 
