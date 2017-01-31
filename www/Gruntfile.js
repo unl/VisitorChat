f1ed60f406604c3272c76d8afdfbf121959aac66
Ryan Dee <ryan@skoolbus39.com>
2017-01-31 13:37:16 -0600

    Install and configure Grunt
    * Installed grunt modules and added to devDependencies in package.json file
    * Configured Grunt tasks in Gruntfile.js

---
+++ www/Gruntfile.js
@@ -0,0 +1,76 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Task configuration.
+        svgmin: {
+            options: {
+                plugins: [
+                    { removeViewBox: false },
+                    { removeUselessStrokeAndFill: false }
+                ]
+            },
+            dist: {
+                files: [{
+                  expand: true,
+                  cwd: 'images/svg/originals',
+                  src: ['*.svg'],
+                  dest: 'images/svg/optimized'
+                }]
+            }
+        },
+        'curl-dir': {
+            mixins: {
+                src: [
+                'https://raw.githubusercontent.com/unl/wdntemplates/master/wdn/templates_4.1/less/_mixins/breakpoints.less',
+                'https://raw.githubusercontent.com/unl/wdntemplates/master/wdn/templates_4.1/less/_mixins/colors.less',
+                'https://raw.githubusercontent.com/unl/wdntemplates/master/wdn/templates_4.1/less/_mixins/fonts.less',
+                'https://raw.githubusercontent.com/unl/wdntemplates/master/wdn/templates_4.1/less/_mixins/functions.less',
+                'https://raw.githubusercontent.com/unl/wdntemplates/master/wdn/templates_4.1/less/_mixins/vars.less'
+                ],
+                dest: 'less/mixins'
+            }
+        },
+        less: {
+            client: {
+                options: {
+                    paths: ['./less'],
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
+                    ]
+                },
+                files: {
+                    'css/VisitorChat/4.0/client.css': 'less/client.less'
+                }
+            },
+            operator: {
+                options: {
+                    paths: ['./less'],
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
+                    ]
+                },
+                files: {
+                    'css/VisitorChat/4.0/operator.css': 'less/operator.less'
+                }
+            }
+        },
+        watch: {
+            svgmin: {
+                files: ['images/svg/originals/*'],
+                tasks: ['svgmin'],
+            },
+            less: {
+                files: ['less/*', 'less/*/*'],
+                tasks: ['curl-dir', 'less'],
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    require('load-grunt-tasks')(grunt);
+
+    // Default task.
+    grunt.registerTask('default', ['watch']);
+};
