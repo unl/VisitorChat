66429ab965e61d1db83c930834626100b7e96562
Michael Fairchild <mfairchild365@gmail.com>
2017-03-03 14:29:37 -0600

    Place the chat widget within a landmark

--- www/js/VisitorChat/4.0/Client.js.php
+++ www/js/VisitorChat/4.0/Client.js.php
@@ -639,7 +639,7 @@ require(['jquery', 'idm', 'analytics'], function($, idm, analytics) {
         },
 
         init:function (serverURL, refreshRate) {
-            $('body').append("" +
+            $('#footer').append("" +
                 "<div id='visitorChat' class='offline' aria-live='polite'>" +
                     "<div id='visitorChat_header' tabindex='0'>" +
                         "<span id='visitorChat_header_text'>Email Us</span>" +
