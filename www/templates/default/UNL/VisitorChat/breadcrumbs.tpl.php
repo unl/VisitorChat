dd2bf65a881ca1c58a935604f1bdd0f53b06cb04
Eric Rasmussen <ericrasmussen1@gmail.com>
2018-10-25 16:33:37 -0500

    Update breadcrumbs

--- www/templates/default/UNL/VisitorChat/breadcrumbs.tpl.php
+++ www/templates/default/UNL/VisitorChat/breadcrumbs.tpl.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="http://www.unl.edu/">UNL</a></li>
+    <li><a href="https://www.unl.edu/">Nebraska</a></li>
     <li><a href="<?php echo \UNL\VisitorChat\Controller::$url ?>">UNLchat</a></li>
     <li><?php echo \UNL\VisitorChat\Controller::$pagetitle ?></li>
 </ul>
