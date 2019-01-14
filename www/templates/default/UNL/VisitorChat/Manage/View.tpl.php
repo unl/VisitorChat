53315d87f4b0bdbecc806bc4dec2270b9be2ec46
Jeffrey Sturek <jsturek8@unl.edu>
2019-01-14 12:09:51 -0600

    Layout changes

--- www/templates/default/UNL/VisitorChat/Manage/View.tpl.php
+++ www/templates/default/UNL/VisitorChat/Manage/View.tpl.php
@@ -1,19 +1,19 @@
 <a id="toggleOperatorStatus" href="#"><span id="currentOperatorStatus"></span></a>
 
 <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-6">
-    <div class="dcf-col-100% dcf-col-25%-start@md" id="visitorChat_clients">
+    <div class="dcf-col-100% dcf-col-25%-start@sm" id="visitorChat_clients">
         <h2 class="dcf-txt-lg">Conversations</h2>
         <div id="clientList"></div>
     </div>
 
-    <div class="dcf-col-100% dcf-col-75%-end@md">
+    <div class="dcf-col-100% dcf-col-75%-end@sm">
       <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-4">
-        <div class="dcf-col-100% dcf-col-67%-start@md" id='clientChatContainer'>
+        <div class="dcf-col-100% dcf-col-67%-start@sm" id='clientChatContainer'>
             <div id="clientChat">
             </div>
         </div>
 
-        <div class="dcf-col-100% dcf-col-33%-end@md  dcf-pr-4" id="clientChatInfoContainer">
+        <div class="dcf-col-100% dcf-col-33%-end@sm  dcf-pr-4" id="clientChatInfoContainer">
             <div id="clientChat_Invitations">
             </div>
         </div>
