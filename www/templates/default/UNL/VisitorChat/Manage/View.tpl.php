613b6920cb3c8e6e2030c45779639a3d5972a1fb
Ryan Dee <ryan@skoolbus39.com>
2019-02-11 16:21:00 -0600

    Adjust chat client colors, background-colors; restore three-column operator dashboard layout (#370)
    * Remove chat widget :hover styles
    * Remove feature query for position
    * Remove chat widget :hover transitions
    * Adjust chat widget colors, background-colors
    * Use utility classes for chat header options
    * Restore three column layout for operator dashboard
    * Ensure links in chat client are scarlet

--- www/templates/default/UNL/VisitorChat/Manage/View.tpl.php
+++ www/templates/default/UNL/VisitorChat/Manage/View.tpl.php
@@ -3,16 +3,16 @@
     <a class="dcf-d-block dcf-btn dcf-btn-primary" id="toggleOperatorStatus" href="#"><span id="currentOperatorStatus"></span></a>
   </div>
   <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-6">
-    <div class="dcf-col-100% dcf-col-33%-start@sm">
-      <div class="dcf-mb-6" id="clientChat_Invitations">
-      </div>
-      <div id="visitorChat_clients">
+    <div class="dcf-col-100% dcf-col-25%-end@sm dcf-2nd@sm" id="clientChat_Invitations">
+    </div>
+    <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-6 dcf-col-100% dcf-col-75%-start@sm">
+      <div class="dcf-col-100% dcf-col-33%-start@sm" id="visitorChat_clients">
         <h2 class="dcf-txt-xs dcf-subhead dcf-bold unl-dark-gray">Conversations</h2>
         <div class="unl-font-sans" id="clientList">
         </div>
       </div>
-    </div>
-    <div class="dcf-col-100% dcf-col-67%-end@sm chat-op-chat unl-font-sans" id='clientChat'>
+      <div class="dcf-col-100% dcf-col-67%-end@sm chat-op-chat unl-font-sans" id='clientChat'>
+      </div>
     </div>
   </div>
 </div>
