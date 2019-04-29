eb30e8092f3d12cf9bd5f0336b991fe49a67d4b3
Jeffrey Sturek <jsturek8@unl.edu>
2019-04-29 09:33:05 -0500

    Remove borders from fieldset and make longstrings without whitespace wrap in chat display

--- www/templates/php/UNL/VisitorChat/Message/RecordList.tpl.php
+++ www/templates/php/UNL/VisitorChat/Message/RecordList.tpl.php
@@ -10,7 +10,7 @@ foreach ($context as $message) {
     
     $messages[$message->id]['message']        = str_replace("&lt;br /&gt;", "<br />", \UNL\VisitorChat\Controller::makeClickableLinks($message->message));
     $messages[$message->id]['date']           = date("g:i:s A", strtotime($message->date_created));
-    $messages[$message->id]['class']          = $message->getDisplayClass();
+    $messages[$message->id]['class']          = $message->getDisplayClass() . " force-wrap";
     $messages[$message->id]['poster']['name'] = $name;
     $messages[$message->id]['poster']['type'] = $poster->type;
 }
