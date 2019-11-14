fa8b6df3ac3dd3b7349334929646453fe13dad89
Ryan Dee <ryan@skoolbus39.com>
2019-11-14 16:23:37 -0600

    Update chat client login markup
    - Uppercase “Optional” to match usage elsewhere
    - Reorder attributes
    - Remove whitespace

--- www/templates/default/UNL/VisitorChat/User/ClientLogin.tpl.php
+++ www/templates/default/UNL/VisitorChat/User/ClientLogin.tpl.php
@@ -2,23 +2,23 @@
     <fieldset class="dcf-m-0 dcf-p-0 dcf-b-0">
         <legend id="visitorChat_footerHeader">Comments for this page</legend>
         <ul class="dcf-list-bare">
-            <li id="visitorChatbot_messageBoxContainer" class="visitorChat_center">
+            <li class="visitorChat_center" id="visitorChatbot_messageBoxContainer">
                 <label for="visitorChat_messageBox">Your Message</label>
-                <textarea class="required-entry" id="visitorChat_messageBox" name="message" rows="3" cols="25" ></textarea>
+                <textarea class="required-entry" id="visitorChat_messageBox" name="message" rows="3" cols="25"></textarea>
             </li>
             <li class="dcf-mt-4 dcf-mb-4 dcf-txt-md dcf-bold" id="visitorChatbot_intent_message" style="display:none">
             </li>
             <li class="visitorChat_info">
-                <label for="visitorChat_name">Your Name (optional)</label>
+                <label for="visitorChat_name">Your Name (Optional)</label>
                 <input id="visitorChat_name" name="name" type="text">
             </li>
             <li class="visitorChat_info">
-                <label for="visitorChat_email">Your Email (optional)</label>
+                <label for="visitorChat_email">Your Email (Optional)</label>
                 <input class="validate-email" id="visitorChat_email" name="email" type="text">
             </li>
             <li class="visitorChat_info">
                 <input id="visitorChat_email_fallback" name="email_fallback" type="checkbox" value="0">
-                <label id="visitorChat_email_fallback_text" for="visitorChat_email_fallback" >I would like a response via email.</label>
+                <label id="visitorChat_email_fallback_text" for="visitorChat_email_fallback">I would like a response via email.</label>
             </li>
         </ul>
     </fieldset>
